28686890c171f0732353dce86bd9cbc47450d897
Civitaspo <civitaspo@gmail.com>
2019-03-07 15:50:12 +0900

    Impl EchoAirflowPlugin

---
+++ airflow/plugin/echo.py
@@ -0,0 +1,21 @@
+import logging
+
+from airflow.plugins_manager import AirflowPlugin
+from airflow.models import BaseOperator
+from airflow.utils.decorators import apply_defaults
+
+
+class EchoOperator(BaseOperator):
+
+    @apply_defaults
+    def __init__(self, message: str):
+        super().__init__()
+        self.message = message
+
+    def execute(self, context) -> None:
+        logging.info(self.message)
+
+
+class EchoAirflowPlugin(AirflowPlugin):
+    name = 'echo'
+    operators = [EchoOperator]
